29eb96faa5da48f49ae26332132b9729c53d42ea
Peter Nagymathe <peter.nagymathe@gmail.com>
2023-06-25 22:15:22 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='rustlite',
+    version='0.1.0',    
+    description='rust like iterators for python',
+    url='https://github.com',
+    author='Peter Nagymathe',
+    author_email='peter@nagymathe.xyz',
+    license='BSD 2-clause',
+    packages=['rustlite'],
+    install_requires=[],
+    classifiers=[
+        'Rust like iterators :: Python :: Rust'
+    ]
+)
